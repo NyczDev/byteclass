9a357fffcfd11691678d5ed08c256515cabffd60
Nicolas Yasumoto <nicolasyasumoto@gmail.com>
2025-04-03 20:10:46 -0300

    feat(Dashboard): :sparkles: - Adiciona layout inicial do dashboard com sidebar e cabeçalho - TODO: Arrumar o layout que esta em coluna para em linha (row)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Dashboard from './Pages/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar /> {/* Sidebar */}
+        
+        <div className="flex-1 flex flex-col">
+          <Header /> {/* Header */}
+
+          <main className="p-6">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   )
 }
 
